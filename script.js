a812d1ebd18a8d7b74124314e99d0791b8883cdb
AartiW-12 <aartiwhaval12@gmail.com>
2025-09-22 11:38:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+const originalColor = "#E6E6FA"
+const hoverColor ="#6F4E37"
+
+squares.forEach((square , index) => {
+            square.addEventListener("mouseover", () =>{
+                squares.forEach((sq, i)=>{
+                    if(i !== index) {
+                        sq.style.backgroundColor = hoverColor;
+                    }
+                })
+            })
+        });
+
+        squares.addEventListener(("mouseout" , () => {
+            squares.forEach((sq) => {
+                sq.style.backgroundColor =originalColor
+            })
+        }))
